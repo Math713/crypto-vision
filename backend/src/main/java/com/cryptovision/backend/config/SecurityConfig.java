506689a0d94f4b38642d704d495b5ee0a7fadcfd
Math713 <matheusrr3004@gmail.com>
2026-06-09 16:14:35 -0300

    feat: implement SecurityConfig to protect available endpoints

---
+++ backend/src/main/java/com/cryptovision/backend/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.cryptovision.backend.config;
+
+import com.cryptovision.backend.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        // .requestMatchers("/admin/**").hasRole("ADMINISTRADOR")
+                        // .requestMatchers("/portfolio/**").hasAnyRole("INICIANTE", "EXPERIENTE", "CORPORATIVO")
+                        // .requestMatchers("/transactions/**").hasAnyRole("INICIANTE", "EXPERIENTE", "CORPORATIVO")
+                        // .requestMatchers("/alerts/**").hasAnyRole("INICIANTE", "EXPERIENTE", "CORPORATIVO")
+                        // .requestMatchers("/cryptos/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
