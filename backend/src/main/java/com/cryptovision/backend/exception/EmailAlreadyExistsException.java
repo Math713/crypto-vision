2ea2d1a8c7e954d49cff38a615668c43051cb231
Math713 <matheusrr3004@gmail.com>
2026-06-12 13:56:41 -0300

    feat: implement exceptions to catch errors

---
+++ backend/src/main/java/com/cryptovision/backend/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.cryptovision.backend.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email already exists: " + email);
+    }
+}
---
+++ backend/src/main/java/com/cryptovision/backend/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cryptovision.backend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String email) {
+    super("User not found with email: " + email);
+  }
+
+  public UserNotFoundException(Long id) {
+    super("User not found with id: " + id);
+  }
+}
